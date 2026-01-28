70f451fe85ca8149afec7361fa50910925445728
Nikolas Tesche <nikolastesche@gmail.com>
2026-01-28 10:27:41 -0300

    Finish Line Material Created

---
+++ Assets/_Game/Gameplay/FinishLine/Materials/FinishLineShaderAux.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FinishLineShaderAux : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+
+    private static readonly int HeightProp = Shader.PropertyToID("_ObjectHeight");
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        _propBlock = new MaterialPropertyBlock();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        UpdateShaderProperties();
+    }
+
+#if UNITY_EDITOR
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateShaderProperties();
+    }
+
+    private void OnValidate()
+    {
+        if (_renderer == null) _renderer = GetComponent<SpriteRenderer>();
+        if (_propBlock == null) _propBlock = new MaterialPropertyBlock();
+
+        UpdateShaderProperties();
+    }
+#endif
+
+    private void UpdateShaderProperties()
+    {
+        if (_renderer == null) return;
+
+        _renderer.GetPropertyBlock(_propBlock);
+
+        float height = _renderer.size.y * transform.localScale.y;
+        _propBlock.SetFloat(HeightProp, height);
+
+        _renderer.SetPropertyBlock(_propBlock);
+    }
+}
